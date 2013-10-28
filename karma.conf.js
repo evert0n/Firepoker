6eeaa97938430f2fe61565e654128a3aacb437f9
Everton Yoshitani <everton@notreve.com>
2013-10-28 12:59:03 +0900

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -50,7 +50,7 @@ autoWatch = true;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Chrome'];
+browsers = ['Firefox'];
 
 // If browser does not capture in given timeout [ms], kill it
 captureTimeout = 5000;
